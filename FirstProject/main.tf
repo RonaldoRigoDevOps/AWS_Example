1b4921a00d317a4b02b034b4cd83ca12fbde8f73
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-29 17:06:49 -0300

    Third sync

--- FirstProject/main.tf
+++ FirstProject/main.tf
@@ -1,76 +1,79 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
   cloud {
-    organization = "ronaldorigo"
+    organization = "REPLACE_ME"
 
     workspaces {
-      name = "ronaldorigo_mylab"
+      name = "gh-actions-demo"
     }
   }
 }
-#Instance parameters
-resource "aws_instance" "MyLab" {
-  #count                       = 1
-  ami                         = "ami-08c40ec9ead489470"
-  instance_type               = "t2.micro"
-  key_name                    = "mylab"
-  security_groups             = [aws_security_group.MyLab_SG.name]
-  associate_public_ip_address = true
-  tags = {
-    Name = "MyLab"
-    # Name = "MyLab-Production${count.index}"
-  }
+
+provider "aws" {
+  region = "us-west-2"
 }
 
-#Security groups parameters
-resource "aws_security_group" "MyLab_SG" {
-  name        = "MyLab_SG"
-  description = "Allow Access By TCP/22 & TCP/80 & TCP/8080"
+resource "random_pet" "sg" {}
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
   ingress {
-    description = "HTTP_8080"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "HTTP_3000"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-  description       = "Allow ping"
-  from_port         = 8
-  to_port           = 0
-  protocol          = "icmp"
-  cidr_blocks       = ["0.0.0.0/00"]
-  }
+}
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "MyLab_SG"
-  }
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
