4ad23707eea2bccb2e6b23c58de89e6c96eda1b6
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-30 11:24:09 -0300

    modification

--- main.tf
+++ main.tf
@@ -35,16 +35,7 @@ resource "aws_instance" "MyLab2" {
   tags = {
     Name = "MyLab2"
   }
-  user_data = <<-EOF
-              #!/bin/bash
-              set -ex
-              sudo yum update -y
-              sudo amazon-linux-extras install docker -y
-              sudo service docker start
-              sudo usermod -a -G docker ec2-user
-              sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-              sudo chmod +x /usr/local/bin/docker-compose
-              EOF
+  user_data	= file("file.sh")
 }
 
 resource "aws_security_group" "MyLab2_SG" {
