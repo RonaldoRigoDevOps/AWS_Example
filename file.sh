9592f71d999107597b027bd5f35069a762579d53
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-30 14:32:04 -0300

    commit

--- file.sh
+++ file.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo docker pull httpd
+sudo apt-get update
+sudo apt install docker.io -y
+sudo docker pull ronaldorigo/mylab:firstproject
+sudo docker run -d -p 3000:3000 ronaldorigo/mylab:firstproject 
+sudo docker pull node
