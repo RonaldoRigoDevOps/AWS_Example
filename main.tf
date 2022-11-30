66101fb47f76836150818ccbacc878ff120ee04e
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-29 23:06:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,11 +35,20 @@ resource "aws_instance" "MyLab2" {
   }
  user_data = <<-EOF
               #!/bin/bash
+              apt-get remove docker docker-engine docker.io containerd runc
               apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
+              apt-get install \
+              ca-certificates \
+              curl \
+              gnupg \
+              lsb-release;
+              mkdir -p /etc/apt/keyrings
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+              echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              apt-get update
+              apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+              docker push ronaldorigo/mylab:firstproject
+              docker run -d -p 80:3000 ronaldorigo/mylab:firstproject
               EOF
 }
 
