77405f549d3fa0385a65e58d001e185ce65e2c5e
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-30 13:58:22 -0300

    Teste modify files

--- file.sh
+++ file.sh
@@ -3,3 +3,4 @@ sudo apt-get update
 sudo apt install docker.io -y
 sudo docker pull ronaldorigo/mylab:firstproject
 sudo docker run -d -p 3000:3000 ronaldorigo/mylab:firstproject 
+sudo docker pull node
--- main.tf
+++ main.tf
@@ -30,76 +30,10 @@ resource "aws_instance" "MyLab2" {
   ami                         = "ami-08c40ec9ead489470"
   instance_type               = "t2.micro"
   key_name                    = "mylab"
-  vpc_security_group_ids      = [aws_security_group.MyLab2_SG.name]
+  vpc_security_group_ids      = "MyLab2_SG"
   associate_public_ip_address = true
   tags = {
     Name = "MyLab2"
   }
   user_data	= file("file.sh")
-}
-
-resource "aws_security_group" "MyLab2_SG" {
-  name        = "MyLab2_SG"
-  description = "Allow Access By TCP/22 & TCP/80 & TCP/8080"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP_8080"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP_3000"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-  description       = "Allow ping"
-  from_port         = 8
-  to_port           = 0
-  protocol          = "icmp"
-  cidr_blocks       = ["0.0.0.0/00"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "MyLab2_SG"
-  }
-}
-
-output "ARN" {
-  value = aws_instance.MyLab2.arn
-}
-output "ID" {
-  value = aws_instance.MyLab2.id
-}
-output "DNS_PUBLIC" {
-  value = aws_instance.MyLab2.public_dns
-}
-output "IP_PUBLIC" {
-  value = aws_instance.MyLab2.public_ip
 }
