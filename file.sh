afad684322376460434dcd56b67de71061e0e6e4
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-30 14:52:50 -0300

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo docker pull node
+sudo apt-get update
+sudo apt install docker.io -y
+sudo docker pull ronaldorigo/mylab:firstproject
+sudo docker run -d -p 3000:3000 ronaldorigo/mylab:firstproject
