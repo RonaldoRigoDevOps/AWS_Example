12d527a22d708d8f96d776fd7d26ca0815e6b79c
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-29 22:07:05 -0300

    teste de commit

--- main.tf
+++ main.tf
@@ -33,14 +33,14 @@ resource "aws_instance" "MyLab2" {
   tags = {
     Name = "MyLab2"
   }
-# user_data = <<-EOF
-#              #!/bin/bash
-#              apt-get update
-#              apt-get install -y apache2
-#              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-#              echo "Hello World" > /var/www/html/index.html
-#              systemctl restart apache2
-#              EOF
+ user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
 }
 
 resource "aws_security_group" "MyLab2_SG" {
