4e671bacf4581f1623e04edba263d0fbd3c0c93e
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-30 11:00:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "MyLab2" {
   ami                         = "ami-08c40ec9ead489470"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.MyLab2_KP.key_name
-  vpc_security_group_ids      = [aws_security_group.MyLab2_SG.name]
+  vpc_security_group_ids      = "mylab"
   associate_public_ip_address = true
   tags = {
     Name = "MyLab2"
