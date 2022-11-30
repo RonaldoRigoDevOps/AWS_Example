0cc1ecfdc763fc6cc393677bc93090b6e951fdf4
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-30 16:21:21 -0300

    Primary Commit

--- main.tf
+++ main.tf
@@ -1,35 +1,22 @@
-# The configuration for the `remote` backend.
-terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "ronaldorigo"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "ronaldo_rigo"
-    }
-  }
-}
-
 #PROVIDER
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "MyLab2" {
+resource "aws_instance" "MyLab_Project1" {
   ami                         = "ami-08c40ec9ead489470"
   instance_type               = "t2.micro"
   key_name                    = "mylab"
-  vpc_security_group_ids      = [aws_security_group.MyLab2_SG.name]
+  vpc_security_group_ids      = [aws_security_group.MyLab_Project1_SG.name]
   associate_public_ip_address = true
   tags = {
-    Name = "MyLab2"
+    Name = "MyLab_Project1"
   }
-  user_data	= file("file.sh")
+  user_data = file("file.sh")
 }
 
-resource "aws_security_group" "MyLab2_SG" {
-  name        = "MyLab2_SG"
+resource "aws_security_group" "MyLab_Project1_SG" {
+  name        = "MyLab_Project1_SG"
   description = "Allow Access By TCP/22 & TCP/80 & TCP/8080"
 
   ingress {
@@ -61,11 +48,11 @@ resource "aws_security_group" "MyLab2_SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-  description       = "Allow ping"
-  from_port         = 8
-  to_port           = 0
-  protocol          = "icmp"
-  cidr_blocks       = ["0.0.0.0/00"]
+    description = "Allow ping"
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/00"]
   }
 
   egress {
@@ -77,19 +64,19 @@ resource "aws_security_group" "MyLab2_SG" {
   }
 
   tags = {
-    Name = "MyLab2_SG"
+    Name = "MyLab_Project1_SG"
   }
 }
 
 output "ARN" {
-  value = aws_instance.MyLab2.arn
+  value = aws_instance.MyLab_Project1.arn
 }
 output "ID" {
-  value = aws_instance.MyLab2.id
+  value = aws_instance.MyLab_Project1.id
 }
 output "DNS_PUBLIC" {
-  value = aws_instance.MyLab2.public_dns
+  value = aws_instance.MyLab_Project1.public_dns
 }
 output "IP_PUBLIC" {
-  value = aws_instance.MyLab2.public_ip
+  value = aws_instance.MyLab_Project1.public_ip
 }
