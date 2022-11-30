14b8cc7abaa45a4b86d663463fecfe341a6cab25
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-11-30 19:57:48 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,9 +3,9 @@ terraform {
   backend "s3" {
     region         = "us-east-1"
     bucket         = "ronaldorigobucket"
-#   dynamodb_table = "ronaldorigobucket-lock"
-#   profile        = ""
-#   role_arn       = ""
+    dynamodb_table = "ronaldorigobucket-lock"
+    profile        = ""
+    role_arn       = ""
     encrypt        = "true"
     key            = "firstbucket/terraform.tfsate"
   }
