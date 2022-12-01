5c8ecc1b930226605ab7e3a7848d5bff1ea31a2a
RonaldoRigoDevOps <119059091+RonaldoRigoDevOps@users.noreply.github.com>
2022-12-01 08:16:02 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "MyLab_Project1" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.MyLab_KP.key_name
+  key_name                    = "mylab" #aws_key_pair.MyLab_KP.key_name
   vpc_security_group_ids      = [aws_security_group.MyLab_Project1_SG.name]
   associate_public_ip_address = true
   tags = {
